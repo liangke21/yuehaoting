3e22e303de5a48e45cce58ab50fa235c45eb0c7e
liangke <1396797522@qq.com>
2021-05-28 12:08:49 +0800

    登录注册完成

---
+++ app/src/test/java/com/example/yuehaoting/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package com.example.yuehaoting
+
+import com.example.yuehaoting.web.Myprocessor
+import org.junit.Test
+
+import org.junit.Assert.*
+import us.codecraft.webmagic.Spider
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+
+    }
+}
